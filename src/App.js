b9b25b5a8b3dcd355bd2c9c722dc841ce859fe85
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-10-22 15:49:31 -0300

    Criação do componente Topo e alteração na funcionalidade

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import "./App.css";
 import { useState } from "react";
+import Topo from "./componentes/Topo";
 import CardAnimal from "./componentes/CardAnimal";
 import CardInformacao from "./componentes/CardInformacao";
 
 export default function App () {
-  let imagemAnimal = "";
-  let informacaoAnimal = "";
-  let tipoAnimal = "Cachorro";
-
   const [tipoDoComponenteCard, setTipoDoComponenteCard] = useState("cachorro");
-
-  const alterarState = () => {
-    if(tipoDoComponenteCard === "cachorro") {
-      setTipoDoComponenteCard("gato");
-    } else {
-      setTipoDoComponenteCard("cachorro");
-    }
-  };
 
   return (
     <div className="App">
-      <header>
-        <h1>Projeto Props + useState</h1>
-        <p>Clique no botão para mudar os componentes abaixo</p>
-        <button onClick={alterarState}>Mudar Animal</button>
-      </header>    
+      <Topo tipoAnimal={tipoDoComponenteCard} setState={setTipoDoComponenteCard}/>
 
       <CardAnimal tipoAnimal = {tipoDoComponenteCard}/> 
 
---
+++ src/componentes/Topo/index.js
@@ -0,0 +1,15 @@
+import './style.css';
+
+export default function Topo ({tipoAnimal, setState}) {
+    const alterarState = () => {
+        tipoAnimal === "cachorro" ? setState("gato") : setState("cachorro");
+    };
+
+    return (
+        <header>
+            <h1>Projeto Props + useState</h1>
+            <p>Clique no botão para mudar os componentes abaixo</p>
+            <button onClick={alterarState}>Mudar Animal</button>
+        </header>    
+    );
+}
